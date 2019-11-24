9caa8bea70c23067674edfdc99cca4d031c73bb7
触手TV紫幕 <45619879+IZIMUI@users.noreply.github.com>
2019-11-25 03:17:44 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,54 +1,32 @@
-const MeoWS = require('meowslib');
-const WSServer = MeoWS.WSServer;
-const BuildSession = MeoWS.BuildSession;
-const Collector = MeoWS.Collector;
-const Commander = MeoWS.Commander;
-const Builder = MeoWS.Builder;
-const Parser = MeoWS.Parser;
-const Lang = MeoWS.Res.Lang;
-const log = MeoWS.Logger;
+const WebSocket = require('ws');
+onst wss = new WebSocket.Server({ port: 8906 });
+const uuid = require('node-uuid');
+// 省略一些参数的定义
+// 服务端处理连接
+wss.on('connection', function(ws) {
+  console.log('client [%s] connected', clientIndex);
+  var connection_uuid = uuid.v4();
+  var nickname = "AnonymousUser" + clientIndex;
+  clientIndex += 1;
+  clients.push({ "id": connection_uuid, "ws": ws, "nickname": nickname });
+  
+ //服务器收到消息时
+  ws.on('message', function(e) {
+   var data = JSON.parse(e);
+   var type = data.type;
+   // 省略业务处理逻辑
+  });
 
-let wss = new WSServer(16384);
-
-console.log('\x1b[33m' + Lang.logo)
-wss.on('client', (session, request) => {
-//init
-	this.CMD = new Commander(session);
-	this.C = new Collector(session);
-	this.B = new Builder(session, this.C);
-
-	log(request.connection.remoteAddress + Lang.connected);
-	session.tellraw(Lang.client_ctd);
-	BuildSession.createAndBind(session);
-	this.C.getPosition().then((v)=>{
-		session.tellraw('Position got: ' + v.join(' '));
-	});
-
-	
-	session.sendCommandSync('testfor @s').then((v) => {
-		session.tellraw(v.victim + Lang.welcome);
-		session.write('§ePowered by CAIMEO.');
-	});
-
-
-	session.on('onMessage',(msg)=>{
-		let config = new Parser(msg,{},Object.keys(this.B.methods));
-		let exec = false;
-		for(let c of config.config){
-			if(c.exec)exec = true;
-		}
-		if(!exec)return;
-		let result = this.CMD.ParseConfig(config,this.B,this.C.config);
-		if(!result.queue){
-			return;
-		}
-		if(result.sync){
-			session.sendCommandQueueSync(result.queue,0,true);
-		}else{
-			session.sendCommandQueue(result.queue,0,true);
-		}
-
-	});
-
-
+  // ws连接关闭时触发的操作
+  ws.on("close", function () {
+   websocketClose();
+  });
+  
+  // 省略函数 websocketClose()、wsSend()、socketClose 的定义
+  // 聊天服务器关闭所触发的操作
+  process.on("SIGINT", function () {
+   console.log("SOCKET CLOSED!");
+   ("客服已关闭，请稍后再来");
+   process.exit();
+  });
 });
