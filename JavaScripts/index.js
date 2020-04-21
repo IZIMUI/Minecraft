e759dd8abbd9159e208234001ce368acfda98f53
触手TV紫幕 <45619879+IZIMUI@users.noreply.github.com>
2020-04-21 16:35:20 +0800

    Update index.js

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -33,7 +33,7 @@ Color: "orange",
 Time:2000
 });
 setTimeout(function() {
-ws = new WebSocket(document.getElementById("ConnectUrl").value)
+ws = new WebSocket("ws://192.168.1.1:4199")//document.getElementById("ConnectUrl").value)
 Connect()
 document.getElementById("loading").style.display = "block";
 }, 2000);
